3885418f71e5c622aa3ff230231b71da117d0c1c
pmaity1995 <prasantamaity549@gmail.com>
2026-06-20 15:51:44 +0530

    Demo_Application_changed

--- app/src/main/java/com/example/demo/DemoApplication.java
+++ app/src/main/java/com/example/demo/DemoApplication.java
@@ -1,5 +1,7 @@
 package com.example.demo;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,17 +11,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class DemoApplication {
 
+    private static final Logger logger = LoggerFactory.getLogger(DemoApplication.class);
+
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
     }
 
     @GetMapping("/")
     public String home() {
+        logger.info("Enterprise product app endpoint was accessed");
         return "Automated Enterprise Product Deployment Platform is running!";
-    }
-
-    @GetMapping("/health")
-    public String health() {
-        return "UP";
     }
 }

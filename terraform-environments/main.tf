f924009dc733bd09a0bc8bb4b263a30fcce10a68
pmaity1995 <prasantamaity549@gmail.com>
2026-06-25 19:04:53 +0530

    Terraform_file_changes_state_versioning

--- terraform-environments/main.tf
+++ terraform-environments/main.tf
@@ -1,3 +1,10 @@
 terraform {
   required_version = ">= 1.0"
+
+  backend "s3" {
+    bucket = "enterprise-product-terraform-state-484826611947"
+    key    = "enterprise-product-platform/terraform.tfstate"
+    region = "us-east-1"
+    encrypt = true
+  }
 }
